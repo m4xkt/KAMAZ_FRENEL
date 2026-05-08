bdd4feb8b04ee8afa1ea3999d521bed93b04d35c
m4xkt <kotkov.ma@phystech.edu>
2026-05-09 00:35:06 +0300

    division without render

---
+++ include/DifrCalc.h
@@ -0,0 +1,36 @@
+// DiffractionCalculator.h
+#ifndef DIFFR_CALC_H
+#define DIFFR_CALC_H
+
+#include <vector>
+#include "fresnel.h"
+
+class DiffractionCalculator {
+public:
+    DiffractionCalculator(double lambda = 515e-9, double deltaLambda = 36e-9);
+    
+    // Параметры физической модели
+    void setWavelength(double lambda_m);
+    void setSpectralWidth(double deltaLambda_m);
+    void setSlitWidth(double width_m);
+    void setDistance(double z_m);
+    
+    // Геттеры
+    double getWavelength() const { return m_lambda; }
+    double getSpectralWidth() const { return m_dLambda; }
+    double getSlitWidth() const { return m_b; }
+    double getDistance() const { return m_z; }
+    
+    // Основной метод расчёта
+    std::vector<double> computeProfile(int numPoints, double xRange_m);
+    
+private:
+    std::vector<double> computeMonochromaticProfile(int numPoints, double xRange_m, double lambda);
+    
+    double m_lambda;      // центральная длина волны [м]
+    double m_dLambda;     // ширина спектра (FWHM) [м]
+    double m_b;           // ширина щели [м]
+    double m_z;           // расстояние до экрана [м]
+};
+
+#endif 
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <opencv2/opencv.hpp>
 #include "FresnelSimulation.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 FresnelSimulation g_sim(515e-9, 36e-9);
 cv::Mat g_image;
-string g_windowName = "Fresnel Diffraction on a Slit";
+const string g_windowName = "Fresnel Diffraction on a Slit";
+
 
 void onSlitChange(int, void*) {
     int pos = cv::getTrackbarPos("Slit width [x0.01 mm]", g_windowName);
@@ -49,9 +51,10 @@ void onResolutionChange(int, void*) {
 }
 
 int main() {
-    cout << "Fresnel Diffraction Simulation\n";
+    cout << "=== Fresnel Diffraction Simulation ===\n";
     cout << "Press ESC to exit.\n";
 
+    // 1. Начальная конфигурация
     g_sim.setImageSize(1400, 900);
     g_sim.setSlitWidth(0.5e-3);
     g_sim.setDistance(0.15);
@@ -59,30 +62,34 @@ int main() {
     g_sim.setZoom(1.0);
     g_sim.setResolution(5.0);
 
+    // 2. Создание окна
     cv::namedWindow(g_windowName, cv::WINDOW_NORMAL);
     cv::resizeWindow(g_windowName, 1400, 900);
 
-    // Трекбары без указателей на переменные (безопасный способ)
+    // 3. Регистрация трекбаров (nullptr -> используем только callback)
     cv::createTrackbar("Slit width [x0.01 mm]", g_windowName, nullptr, 490, onSlitChange);
-    cv::createTrackbar("Distance [x0.5 cm]", g_windowName, nullptr, 198, onDistanceChange);
-    cv::createTrackbar("Brightness [x0.1]", g_windowName, nullptr, 49, onIntensityChange);
-    cv::createTrackbar("Zoom [x0.05]", g_windowName, nullptr, 100, onZoomChange);
-    cv::createTrackbar("Resolution [um]", g_windowName, nullptr, 100, onResolutionChange);
+    cv::createTrackbar("Distance [x0.5 cm]",     g_windowName, nullptr, 198, onDistanceChange);
+    cv::createTrackbar("Brightness [x0.1]",      g_windowName, nullptr,  49, onIntensityChange);
+    cv::createTrackbar("Zoom [x0.05]",           g_windowName, nullptr, 100, onZoomChange);
+    cv::createTrackbar("Resolution [um]",        g_windowName, nullptr, 100, onResolutionChange);
 
-    // Установка начальных значений трекбаров
+    // 4. Установка начальных позиций (должны совпадать с параметрами в п.1)
     cv::setTrackbarPos("Slit width [x0.01 mm]", g_windowName, 40);   // 0.5 мм
-    cv::setTrackbarPos("Distance [x0.5 cm]", g_windowName, 28);       // 15 см
-    cv::setTrackbarPos("Brightness [x0.1]", g_windowName, 9);         // 1.0
-    cv::setTrackbarPos("Zoom [x0.05]", g_windowName, 16);             // 1.0
-    cv::setTrackbarPos("Resolution [um]", g_windowName, 8);           // 5 мкм
+    cv::setTrackbarPos("Distance [x0.5 cm]",     g_windowName, 28);  // 15.0 см
+    cv::setTrackbarPos("Brightness [x0.1]",      g_windowName,  9);  // 1.0x
+    cv::setTrackbarPos("Zoom [x0.05]",           g_windowName, 16);  // 1.0x
+    cv::setTrackbarPos("Resolution [um]",        g_windowName,  8);  // 5.0 мкм
 
+    // 5. Первая отрисовка
     g_image = g_sim.generateImage();
     cv::imshow(g_windowName, g_image);
 
+    // 6. Главный цикл обработки событий
     while (true) {
         int key = cv::waitKey(30);
-        if (key == 27) break;
+        if (key == 27) break; 
     }
+
     cv::destroyAllWindows();
     return 0;
 }
